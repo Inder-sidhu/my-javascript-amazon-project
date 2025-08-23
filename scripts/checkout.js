e7da67a9c59725f027b6285aa852d4112a727413
Inder-sidhu <sidhuinder0008@gmail.com>
2025-08-23 13:56:20 +0530

    initial commit

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
-import {loadCart} from '../data/cart.js';
+import {loadCart, cart} from '../data/cart.js';
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
@@ -23,6 +24,7 @@ async function loadPage() {
     console.log('Unexpected error. Please try again later.');
   }
 
+  renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 }
